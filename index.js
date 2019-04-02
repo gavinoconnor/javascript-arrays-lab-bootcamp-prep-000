919c1060884948560cfcf419cf37e96ac4cdc658
Gavin O'Connor <gedmund@gmail.com>
2019-04-02 20:54:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,37 +3,34 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 // Add your functions and code here
 
 var destructivelyAppendKitten = function(newCat) {
-  kittens.push(newCat)
-  return kittens
+  kittens.push(newCat);
+  return kittens;
 }
 
-var destructivelyPrependKitten = function(newCat) {
-  kittens.unshift(newCat)
-  return kittens
+var destructivelyPrependKitten = function() {
+
 }
 
 var destructivelyRemoveLastKitten = function() {
-  kittens.pop()
-  return kittens
+  
 }
 
 var destructivelyRemoveFirstKitten = function() {
-  kittens.shift()
-  return kittens
+  
 }
 
-var appendKitten = function(newCat) {
-  return [...kittens, newCat]
+var appendKitten = function() {
+
 }
 
-var prependKitten = function(newCat) {
-  return [newCat, ...kittens]
+var prependKitten = function() {
+  
 }
 
 var removeLastKitten = function() {
-  return kittens.slice(0, kittens.length -1)
+  
 }
 
 var removeFirstKitten = function() {
-  return kittens.slice(1)
+  
 }
